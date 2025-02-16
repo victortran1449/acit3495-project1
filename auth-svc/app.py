1f00f6ab57dd8ffbf704299e9a61085dfc679ca3
victortran1449@gmail.com <victortran1449@gmail.com>
2025-02-16 00:22:14 -0800

    add auth-svc and enterdata-app

---
+++ auth-svc/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, url_for, redirect, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
+import os
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class Users(UserMixin, db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(250), unique=True,
+						nullable=False)
+	password = db.Column(db.String(250),
+						nullable=False)
+
+db.init_app(app)
+
+with app.app_context():
+	db.create_all()
+
+@login_manager.user_loader
+def loader_user(user_id):
+    return Users.query.get(user_id)
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+	if request.method == "POST":
+		user = Users(username=request.form.get("username"),
+					password=request.form.get("password"))
+		db.session.add(user)
+		db.session.commit()
+		return redirect(url_for("login"))
+	return render_template("sign_up.html")
+    
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    redirect_url = request.args.get('redirect') or url_for('home')
+    if request.method == "POST":
+        user = Users.query.filter_by(username=request.form.get("username")).first()
+        if user is None:
+            return render_template("login.html")
+        if user.password == request.form.get("password"):
+            login_user(user)
+            print(redirect_url)
+            response = make_response(redirect(redirect_url))
+            return redirect(redirect_url)
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/is_auth")
+def is_auth():
+    return jsonify({"is_auth": current_user.is_authenticated})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
---
+++ enterdata-app/app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const port = 4000;
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+app.set('view engine', 'pug');
+
+// Create MySQL connection
+const db = mysql.createConnection({
+  host: 'player-db',
+  port: 3306,
+  user: process.env.MYSQL_USER, 
+  password: process.env.MYSQL_PASSWORD, 
+  database: process.env.MYSQL_DATABASE
+});
+
+// Connect to MySQL
+db.connect(err => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
+
+// Route to insert data
+app.post('/enter_data', (req, res) => {
+  const { name, points } = req.body;
+  if (!name || !points) {
+    return res.status(400).json({ error: 'Invalid data' });
+  }
+
+  const sql = 'INSERT INTO players (name, points) VALUES (?, ?)';
+  db.query(sql, [name, points], (err, result) => {
+    if (err) return res.status(500).json({ error: err.message });
+    console.log(`Player data entered, playerId :${result.insertId}`);
+    res.redirect("/")
+  });
+});
+
+app.get('/', async (req, res) => {
+    try {
+        const response = await fetch("http://auth-svc:5000/is_auth", {
+            method: "GET",
+            headers: {
+                "Cookie": `session=${req.cookies.session}`
+            }
+        });
+        if (!response.ok) {
+            throw new Error("Failed to request auth status");
+        }
+        const data = await response.json()
+        console.log(data)
+        if (data.is_auth) {
+            res.render('enter_data');
+        } else {
+            res.redirect(`http://localhost:5000/login?redirect=http://localhost:${port}`);
+        }
+    } catch (err) {
+        console.error('Err getting auth status', err);
+        res.status(500).send('Err getting auth status');
+    }
+  });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
